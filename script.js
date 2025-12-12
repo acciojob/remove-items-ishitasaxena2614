55de897ff066503f77766602a804a04b285735ef
ishitasaxena2614 <ishitasaxena324@gmail.com>
2025-12-12 23:28:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+const btn = document.querySelector('input[type="button"]');
+const select = document.getElementById("colorSelect");
+
+btn.addEventListener("click", () => {
+  // Get selected index
+  const index = select.selectedIndex;
+
+  // Remove option if any selected
+  if (index >= 0) {
+    select.remove(index);
+  }
+});
+
